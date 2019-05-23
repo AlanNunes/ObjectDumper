7f1a55722f335af8627609c87c3117741ea3ba9e
Alan Nunes <alannunes@corpugbvr.ugb.edu.br>
2019-05-23 11:32:12 -0300

    Create Object Dumper

---
+++ ObjectDumper/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BodyType bodyType = new BodyType()
+            {
+                Name = "Body Builder"
+            };
+            Person bambam = new Person()
+            {
+                Name = "Bambam",
+                Age = 37,
+                BodyType = bodyType
+            };
+            string result = "";
+            ObjectDumper.Dump(bambam, ref result, 3);
+            Console.Write(result);
+            Console.Read();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public BodyType BodyType { get; set; }
+        public void NoWaterWithMuscles()
+        {
+            Console.WriteLine("It's no water with muscles!!!");
+        }
+    }
+    class BodyType
+    {
+        public string Name { get; set; }
+    }
+}
